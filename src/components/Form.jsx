78a204543f035fbf226bf1c424d03abda45cfc50
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-01-20 22:37:11 +0530

    search country

---
+++ src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Country from "./Country.json";
+const Form = () => {
+    let data = Country;
+    return (
+        <section className="form-block">
+            <form>
+                <label htmlFor="search"> Search Country </label>
+                <input 
+                type="text"
+                 placeholder="Search here....." 
+                 id="search" 
+                 list="country"
+                 />
+
+                <datalist id="counrty">
+                    {data.map(value => {
+                            return (
+                                <option value={value.code} key={value.name}>
+                                    {value.name}
+                                </option>   
+                            );
+                        })}
+                </datalist>
+                <button>submit</button>
+            </form>
+        </section>
+    );
+};
+
+export default Form;
